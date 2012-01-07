ae60f2e35a64e67bab3f77dd27f24b78fb99c316
Gregor Münster <github@muensters.org>
2012-01-07 13:30:55 +0100

    SOAP

--- verwaltung.php
+++ verwaltung.php
@@ -10,3 +10,54 @@ Die geheime Verwaltung.<br />
 	<li><a href="verwaltung.php<?php session::showLink(true);?>">Verwaltung</a></li>
 	<li><?php $mylogin->showLogout();?></li>
 </ul>
+
+<?php
+
+$options = array('encoding'           => 'UTF-8',
+                 'connection_timeout' => 5,
+                 'exceptions'         => 1,
+                 );
+
+$location = 'http://www.OpenLigaDB.de/Webservices/Sportsdata.asmx?WSDL';
+
+try
+{
+    $client = new SoapClient($location, $options);
+    $params = new stdClass;
+    $params->MatchID = 626;
+    $response = $client->GetMatchByMatchID($params);
+}
+catch (SoapFault $e)
+{
+    die($e->faultcode . ': ' . $e->faultstring);
+}
+catch (Exception $e)
+{
+    die($e->getCode() . ': ' . $e->getMessage());
+}
+echo "<pre>";
+print_r($response->GetMatchByMatchIDResult);
+echo "</pre>";
+
+echo '<img src="'.$response->GetMatchByMatchIDResult->iconUrlTeam1.'"> '.$response->GetMatchByMatchIDResult->pointsTeam1.' : '.$response->GetMatchByMatchIDResult->pointsTeam2.' <img src="'.$response->GetMatchByMatchIDResult->iconUrlTeam2.'">';
+
+
+try
+{
+	$client2 = new SoapClient($location, $options);
+	$params2 = new stdClass;
+	$params2->MatchID = 627;
+	$response2 = $client2->GetMatchByMatchID($params2);
+}
+catch (SoapFault $e)
+{
+	die($e->faultcode . ': ' . $e->faultstring);
+}
+catch (Exception $e)
+{
+	die($e->getCode() . ': ' . $e->getMessage());
+}
+
+echo "<pre>";
+print_r($response2->GetMatchByMatchIDResult);
+echo "</pre>";
