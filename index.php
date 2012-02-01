3601ed04b536ed141245902133e29a8dc3a398e0
Anas <incognitanas@gmail.com>
2012-02-01 22:42:00 +0100

    Index- und Willkommensseite überarbeitet Registrierung Bug behoben

--- index.php
+++ index.php
@@ -7,13 +7,14 @@ include_once 'header.php';
 ?>
 <div style="margin-left:95%"><?php $mylogin->showLogout();?></div>
 <h1>Willkommen <?php echo $mylogin->getUserName();?>!</h1>
-<h2>Index Seite</h2>
-Die geheime Index-Seite.<br />
+<h2>Tippspiel AG</h2>
+Wetten, dass...<br />
 <br />
 <h3>Navigation</h3>
 <ul>
 	<li><a href="index.php<?php session::showLink(true);?>">Startseite</a></li>
 	<li><a href="tippeingabe.php<?php session::showLink(true);?>">Tippeingabe</a></li>
+	<li><a href="ranking.php<?php session::showLink(true);?>">Statistik</a></li>
 	
 </ul>
 </body>
--- willkommen.php
+++ willkommen.php
@@ -7,12 +7,13 @@ include_once 'header.php';
 ?>
 <div style="margin-left:95%"><?php $mylogin->showLogout();?></div>
 <h1>Willkommen <?php echo htmlspecialchars($_GET['username']);?>!</h1>
-<h2></h2>Die geheime Willkommen-Seite.</h2><br />
+<h2>Tippspiel AG</h2><br />
+Wetten, dass...<br />
 <br />
 <h3>Navigation</h3>
 <ul>
 	<li><a href="index.php<?php session::showLink(true);?>">Startseite</a></li>
 	<li><a href="tippeingabe.php<?php session::showLink(true);?>">Tippeingabe</a></li>
-	<!--<li><?php $mylogin->showLogout();?></li>-->
+	<li><a href="ranking.php<?php session::showLink(true);?>">Statistik</a></li>
 </ul>
 
